1629b22f2d501ac33f652b5fee79e7fcef3fa7b9
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-05-15 12:11:45 +0200

    testing with grid functions

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const container = document.querySelector(".container");
+const container = document.getElementById("container");
 
 function makeGrid(rows, cols) {
-    for(let i = 0; i < (rows * cols); i++){
-        
-    }
-}
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = `grid-item${c}`;
+  };
+  for(i = 0; i < (rows * cols); i++){
+    grid.style.setProperty('padding: 10px');
+    grid.style.setProperty('border: 1px solid #ddd;');
+  }
+};
+
+makeGrid(16, 16);
+
+
