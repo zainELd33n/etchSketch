ea90fb6289f81ffb26c0e4b6e4123751a712a92c
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-05-17 11:28:11 +0200

    added reset button and sketching func

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const container = document.getElementById("container");
+const reset = document.getElementById("reset");
 
 function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = `grid-item${c}`;
+    let grid = container.appendChild(cell);
+    grid.className = `grid-item`;
+    grid.setAttribute('id', `${c}`);
   };
-  for(i = 0; i < (rows * cols); i++){
-    grid.style.setProperty('padding: 10px');
-    grid.style.setProperty('border: 1px solid #ddd;');
-  }
 };
 
 makeGrid(16, 16);
 
+container.addEventListener('mouseover', (e) => {
+  let target = e.target;
+  let clicked = target.id;
+  if(target != container) {
+  document.getElementById(`${clicked}`).style.backgroundColor="#574c4c";
+  };
+});
 
+reset.addEventListener('click', (e) => {
+  location.reload();
+})
